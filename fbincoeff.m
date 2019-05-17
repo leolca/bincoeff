fa89fe6ccc106a29725dddd043bb492c58b807ac
Leonardo Araujo <leolca@gmail.com>
2019-05-17 16:58:47 -0300

    patch for n=0

--- fbincoeff.m
+++ fbincoeff.m
@@ -1,7 +1,11 @@
 function c = fbincoeff(n,k), 
 
-  l = [1:floor(n/3)+1]';
-  c = round( (2^n/(n+1)) * (1+2*sum(cos(pi*l/(n+1)).^n.*cos(pi*l.*(2*k-n)./(n+1)), 1)) );
+  if n > 0,
+    l = [1:floor(n/3)+1]';
+    c = round( (2^n/(n+1)) * (1+2*sum(cos(pi*l/(n+1)).^n.*cos(pi*l.*(2*k-n)./(n+1)), 1)) );
+  else
+    c = 1;
+  endif
 
 endfunction
 
--- fftbincoeff.m
+++ fftbincoeff.m
@@ -1,4 +1,5 @@
 function c = fftbincoeff(n,k)
   c = round(real(ifft(fft( [1 1 zeros(1,n-1)] ).^n))); 
+  c = c(1:n+1); % fix for n=0
   if nargin > 1, c = c(k+1); endif
 endfunction
