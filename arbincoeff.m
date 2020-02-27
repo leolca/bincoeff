bb4058ab081f5041cd9480af28fdd9d2a4566659
Leonardo Araujo <leolca@gmail.com>
2020-02-27 11:11:41 -0300

    acelerated methods using known values

--- arbincoeff.m
+++ arbincoeff.m
@@ -7,10 +7,10 @@ function c = arbincoeff (n,k,known),
    else
       n0 = length(known)-1;
       m = n-n0;
-      if k-m+1 > 0,
-         c = round (prod([n:-1:n0+1] ./ [k:-1:k-m+1]) * known(k-m+1));
+      if m == 0,
+	 c = known(k+1);
       else
-	 c = round (prod([n:-1:n-k+1] ./ [k:-1:1]));
+	 c = round( (n/k)*arbincoeff (n-1,k-1,known) );
       endif
    endif
 
